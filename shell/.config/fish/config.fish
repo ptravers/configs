b177c87750c3dfa1d4f30264c239f97978c8d796
Peter Travers <traverspw@gmail.com>
2025-10-22 11:26:25 -0400

    Fix fish config merge conflict
    Resolve merge conflict markers in fish_prompt and fish_greeting functions.
    Keep the updated version with hostnamectl and simpler disk/network display.

--- shell/.config/fish/config.fish
+++ shell/.config/fish/config.fish
@@ -84,36 +84,6 @@ function fish_user_key_bindings
 end
 
 function fish_prompt
-<<<<<<< HEAD
-	set_color brblack
-	echo -n "["(date "+%H:%M")"] "
-	set_color blue
-	echo -n (command -q hostname; and hostname; or hostnamectl hostname)
-	if [ $PWD != $HOME ]
-		set_color brblack
-		echo -n ':'
-		set_color yellow
-		echo -n (basename $PWD)
-	end
-	set_color green
-	printf '%s ' (__fish_git_prompt)
-	set_color red
-	echo -n '| '
-	set_color normal
-end
-
-function fish_greeting
-	echo
-	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
-	echo -e " \\e[1mDisk usage:\\e[0m"
-	echo
-	echo -ne (\
-		df -l -h | grep -E 'dev/(xvda|sd|mapper|nvme)' | \
-		awk '{printf "\\\\t%s\\\\t%4s / %4s  %s\\\\n\n", $6, $3, $2, $5}' | \
-		sed -e 's/^\(.*\([8][5-9]\|[9][0-9]\)%.*\)$/\\\\e[0;31m\1\\\\e[0m/' -e 's/^\(.*\([7][5-9]\|[8][0-4]\)%.*\)$/\\\\e[0;33m\1\\\\e[0m/' | \
-=======
     set_color brblack
     echo -n "["(date "+%H:%M")"] "
     set_color blue
@@ -141,45 +111,10 @@ function fish_greeting
     echo -ne (\
 		df -l -h | grep -E 'dev/(nvme|sdb)' | \
 		awk '{printf "\\\\t%s / %4s %4s  %s\\\\n\n", $3, $2, $5, $6}' | \
->>>>>>> 1ccbb09 (Add helix config and update shell configurations)
 		paste -sd ''\
 	)
     echo
 
-<<<<<<< HEAD
-	if command -q ip
-		echo -e " \\e[1mNetwork:\\e[0m"
-		echo
-		# http://tdt.rocks/linux_network_interface_naming.html
-		echo -ne (\
-			ip addr show up scope global | \
-				grep -E ': <|inet' | \
-				sed \
-					-e 's/^[[:digit:]]\+: //' \
-					-e 's/: <.*//' \
-					-e 's/.*inet[[:digit:]]* //' \
-					-e 's/\/.*//'| \
-				awk 'BEGIN {i=""} /\.|:/ {print i" "$0"\\\n"; next} // {i = $0}' | \
-				sort | \
-				column -t -R1 | \
-				# public addresses are underlined for visibility \
-				sed 's/ \([^ ]\+\)$/ \\\e[4m\1/' | \
-				# private addresses are not \
-				sed 's/m\(\(10\.\|172\.\(1[6-9]\|2[0-9]\|3[01]\)\|192\.168\.\).*\)/m\\\e[24m\1/' | \
-				# unknown interfaces are cyan \
-				sed 's/^\( *[^ ]\+\)/\\\e[36m\1/' | \
-				# ethernet interfaces are normal \
-				sed 's/\(\(en\|em\|eth\)[^ ]* .*\)/\\\e[39m\1/' | \
-				# wireless interfaces are purple \
-				sed 's/\(wl[^ ]* .*\)/\\\e[35m\1/' | \
-				# wwan interfaces are yellow \
-				sed 's/\(ww[^ ]* .*\).*/\\\e[33m\1/' | \
-				sed 's/$/\\\e[0m/' | \
-				sed 's/^/\t/' \
-			)
-		echo
-	end
-=======
     echo -e " \\e[1mNetwork:\\e[0m"
     echo
     # http://tdt.rocks/linux_network_interface_naming.html
@@ -210,7 +145,6 @@ function fish_greeting
 			sed 's/^/\t/' \
 		)
     echo
->>>>>>> 1ccbb09 (Add helix config and update shell configurations)
 
     set r (random 0 100)
     if [ $r -lt 5 ] # only occasionally show backlog (5%)
